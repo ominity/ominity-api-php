8277b55aa1e9f34dcd507fffa6dc5e9974381629
Tim De Smet <tim.desmet@wizzou.com>
2025-06-11 21:17:16 +0200

    Revert updated version

--- src/OminityApiClient.php
+++ src/OminityApiClient.php
@@ -18,7 +18,7 @@ class OminityApiClient
     /**
      * Version of our client.
      */
-    public const CLIENT_VERSION = "1.1.37";
+    public const CLIENT_VERSION = "1.1.36";
 
     /**
      * Endpoint of the remote API.
